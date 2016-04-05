8712bb25f9fae88821755a2397fd6d12b6843ee8
hushulin12321@126.com <hushulin12321@126.com>
2016-04-05 09:30:22 +0800

    0

---
+++ plugins/hushulin/kaide/Plugin.php
@@ -0,0 +1,34 @@
+<?php namespace Hushulin\Kaide;
+
+/**
+ * The plugin.php file (called the plugin initialization script) defines the plugin information class.
+ */
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Kaide后台',
+            'description' => 'kaide后台系统，包括用户，水表，缴费，通知等模块',
+            'author'      => 'Hushulin',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    public function registerNavigation()
+    {
+        return [
+            'kaide' => [
+                'label'       => 'Kaide后台',
+                'url'         => Backend::url('hushulin/kaide/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['hushulin.kaide.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+}
