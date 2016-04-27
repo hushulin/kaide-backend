4392329daa5ce47ac3a59f83f2b1c7f6ed0c0d32
hushulin12321@126.com <hushulin12321@126.com>
2016-04-27 20:39:27 +0800

    0

--- modules/kaide/ServiceProvider.php
+++ modules/kaide/ServiceProvider.php
@@ -1,10 +1,17 @@
 <?php
 namespace Kaide;
 use October\Rain\Support\ModuleServiceProvider;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Aysheka\Socket\Client\Client;
+use Aysheka\Socket\Address\IP4;
+use Aysheka\Socket\Type\Stream;
+use Aysheka\Socket\Transport\TCP;
 
 class ServiceProvider extends ModuleServiceProvider {
     public function register() {
-        // code...
+        App::singleton('kaide.sock', function () {
 
+            return new Client('127.0.0.1', 8089, new IP4() , new Stream() , new TCP() , new EventDispatcher());
+        });
     }
 }
