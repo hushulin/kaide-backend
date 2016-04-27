f59a9d4212e6dade7797b81971308105c90f4ee8
hushulin12321@126.com <hushulin12321@126.com>
2016-04-27 17:27:53 +0800

    0

---
+++ modules/kaide/ServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+namespace Kaide;
+use October\Rain\Support\ModuleServiceProvider;
+
+class ServiceProvider extends ModuleServiceProvider {
+    public function register() {
+        // code...
+
+    }
+}
