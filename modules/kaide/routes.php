b59830accbd06ec32aebab98a4ecc9e1a628991b
hushulin12321@126.com <hushulin12321@126.com>
2016-04-28 09:46:51 +0800

    0

--- modules/kaide/routes.php
+++ modules/kaide/routes.php
@@ -1,14 +1,4 @@
 <?php
-Route::get('/sock' , function(){
-	$sock = App::make('kaide.sock');
-
-	var_dump($sock);
-
-	var_dump($sock->connect());
-
-	$read = $sock->read();
-
-	var_dump($read);
-
-	var_dump($sock);
+Route::get('/sock', function () {
+    $sock = App::make('kaide.sock');
 });
