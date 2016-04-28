dcea92d1f2926a449dac4ccffa1eca84daf55aad
hushulin12321@126.com <hushulin12321@126.com>
2016-04-28 14:53:01 +0800

    0

--- modules/kaide/routes.php
+++ modules/kaide/routes.php
@@ -2,8 +2,5 @@
 Route::get('/sock', function () {
     $sock = App::make('kaide.sock');
     $sock->connect();
-    $sock->read();
     $sock->write('Request');
-    $sock->read();
-    $sock->close();
 });
