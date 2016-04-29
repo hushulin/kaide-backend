a7493c2c30e41d49490ae7ba14baddd119e58595
hushulin12321@126.com <hushulin12321@126.com>
2016-04-29 16:04:16 +0800

    0

--- modules/system/console/KaideSwoole.php
+++ modules/system/console/KaideSwoole.php
@@ -25,7 +25,7 @@ class KaideSwoole extends Command {
      */
     public function fire() {
         $this->output->writeln('<info>Kaide swoole starting ... </info>');
-        $serv = new swoole_server("0.0.0.0", 9501);
+        $serv = new \swoole_server("0.0.0.0", 9501);
         $serv->set(array(
             'worker_num' => 8, //工作进程数量
             'daemonize' => true, //是否作为守护进程
