e2219a82e78e0383eafeb1483a2d7d958adc1700
hushulin12321@126.com <hushulin12321@126.com>
2016-04-29 15:59:13 +0800

    0

--- modules/system/console/KaideSwoole.php
+++ modules/system/console/KaideSwoole.php
@@ -25,6 +25,23 @@ class KaideSwoole extends Command {
      */
     public function fire() {
         $this->output->writeln('<info>Kaide swoole starting ... </info>');
+        $serv = new swoole_server("0.0.0.0", 9501);
+        $serv->set(array(
+            'worker_num' => 8, //工作进程数量
+            'daemonize' => true, //是否作为守护进程
+
+        ));
+        $serv->on('connect', function ($serv, $fd) {
+            echo "Client:Connect.\n";
+        });
+        $serv->on('receive', function ($serv, $fd, $from_id, $data) {
+            $serv->send($fd, 'Swoole: ' . $data);
+            $serv->close($fd);
+        });
+        $serv->on('close', function ($serv, $fd) {
+            echo "Client: Close.\n";
+        });
+        $serv->start();
     }
     /**
      * Get the console command arguments.
